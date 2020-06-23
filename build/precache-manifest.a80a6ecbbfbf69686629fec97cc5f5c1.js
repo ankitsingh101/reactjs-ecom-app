7ef43419b9134f3c75a1941730102e2b8a4ae89e
ankitsingh101 <ankitsingh.udaipur@gmail.com>
2020-06-23 15:46:15 +0000

    Save before ejecting

---
+++ build/precache-manifest.a80a6ecbbfbf69686629fec97cc5f5c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd7bb4f7ce5873811ea95910bcdc00f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ca75fa56a726b34659a",
+    "url": "/static/css/main.122a13d4.chunk.css"
+  },
+  {
+    "revision": "1fff59375ce18e28dcc9",
+    "url": "/static/js/2.59de6280.chunk.js"
+  },
+  {
+    "revision": "7ca75fa56a726b34659a",
+    "url": "/static/js/main.1720d895.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  }
+]);
